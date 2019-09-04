c1482a86821c35239a4c55a5a50ad1cc581d79c8
muthy <muthy.afifah@gdis-world.com>
2019-09-04 10:10:02 +0700

    change server

--- src/index.js
+++ src/index.js
@@ -8,17 +8,21 @@ import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 );
 
 
 ReactDOM.render(app, container);
 
-
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// console.log(module.hot);
+// if(module.hot) {
+//   console.log('ehehe')
+//   module.hot.accept('./Application/App', () => ReactDOM.render(require('./Application/App').default))
+// }
