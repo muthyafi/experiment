e033cd133d3051d1e69b3ec61cafb2ff033ae338
muthy <muthy.afifah@gdis-world.com>
2019-09-20 15:17:49 +0700

    styled-components

--- src/Application/App.js
+++ src/Application/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../global';
+import { theme } from '../theme';
 import Home from './Home';
 import About from './About';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route pah="/about" component={About} />
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </>
+    </ThemeProvider>
   ); 
 }
 
