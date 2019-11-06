c16bf9a8e0510e9e11944ee1d732b7063ccf538c
muthy <muthy.afifah@gdis-world.com>
2019-11-06 15:00:26 +0700

    close outside menu

--- src/Application/App.js
+++ src/Application/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Route, Switch} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../global';
@@ -10,6 +10,22 @@ import About from './About';
 
 function App() {
   const [open, setOpen] = useState(false);
+  const node = useRef();
+  
+  useEffect(() => {
+    const listener = event => {
+      if(!node.current || node.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+
+    document.addEventListener('mousedown', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    }
+  })
+
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -20,7 +36,7 @@ function App() {
             <Route path="/about" component={About} />
           </Switch>
         </StyledPageContainer>
-        <div>
+        <div ref={node}>
           <Burger open={open} setOpen={setOpen}/>
           <Menu open={open} setOpen={setOpen} />
         </div>
